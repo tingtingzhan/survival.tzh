0235535a9ca763ed3fd19c0d4fb7a0b54fc0c674
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-02 12:45:00 -0500

    test

--- R/survdiff.R
+++ R/survdiff.R
@@ -1,9 +1,25 @@
 
 #' @importFrom stats nobs
+#' @export nobs.survdiff
 #' @export
 nobs.survdiff <- function(object, ...) sum(object$n)
 # ?broom:::nobs.survdiff (2024-09-26) is WRONG!!!
 
+
+
+#' @title Get \eqn{p}-values from \link[survival]{survdiff} Object
+#' 
+#' @param x \link[survival]{survdiff} object
+#' 
+#' @examples
+#' survdiff(Surv(futime, fustat) ~ rx,data=ovarian) |>
+#'  .pval.survdiff()
+#' 
+#' @importFrom stats pchisq
+#' @export
+.pval.survdiff <- function(x) {
+  pchisq(x$chisq, df = length(x$n) - 1L, lower.tail = FALSE)
+}
 
 
 
