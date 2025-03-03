ad5e990790f181e1b1c7b6519177020bfad42052
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-02 22:41:36 -0500

    test

--- R/cox.zph.R
+++ R/cox.zph.R
@@ -4,6 +4,9 @@
 #' 
 #' @param x a \link[survival]{cox.zph} object
 #' 
+#' @returns
+#' Function [.pval.cox.zph] returns a \link[base]{double} \link[base]{vector}.
+#' 
 #' @examples
 #' ?survival::cox.zph
 #' m = coxph(Surv(futime, fustat) ~ age + ecog.ps, data = ovarian) |> cox.zph() 
@@ -12,9 +15,13 @@
 #' @export
 .pval.cox.zph <- function(x) {
   if (!any(x$transform == c('log', 'km'))) stop('cox.zph transform is neither of log/km')
-  x$table[, 'p']
+  cf <- x$table
+  ret <- cf[, 'p']
+  names(ret) <- rownames(cf) # nrow-1 drops rownames
+  return(ret)
 }
 
-#' @rdname S3_coxzph
-#' @export
-desc_.cox.zph <- function(x) 'Test Proportional Hazards Assumption of Cox Regression'
+# @rdname S3_coxzph
+# @export
+# enduser wont interact with 'cox.zph'
+#desc_.cox.zph <- function(x) 'Test Proportional Hazards Assumption of Cox Regression'
