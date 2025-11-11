9b81b80f46fa35ddd6aa0a956b1fbb8c87302c9d
Tingting Zhan <tingtingzhan@gmail.com>
2025-11-11 10:01:58 -0500

    tzh

--- R/survfit.R
+++ R/survfit.R
@@ -5,6 +5,40 @@
 #' 
 #' @param object \link[survival]{survfit.object}
 #' 
+# @param ribbon \link[base]{logical} scalar, default `TRUE`
+#' 
+# @param labels (optional) \link[base]{character} \link[base]{vector}
+#' 
+# @param oneminus \link[base]{logical} scalar
+#' 
+#' @param ... additional parameters of function [autolayer.summary.survfit()]
+#' 
+#' @note
+#' It's very difficulty to grab the time \link[base]{units} of the response of a 
+#' \link[survival]{survfit} or \link[survival]{summary.survfit} object.
+#' The only way is `eval(object$call$formula[[2L]], envir = eval(object$call$data))`,
+#' which is very vulnerable as `object$call$data` might be defined in some other internal functions.
+#' Therefore we leave the specification of `xlab` (ideally the time unit) to the end user.
+#' 
+#' @keywords internal
+#' @importFrom ggplot2 autolayer
+#' @export autolayer.survfit
+#' @export
+autolayer.survfit <- function(object, ...) {
+  object |> 
+    summary(censored = TRUE) |> # ?survival:::summary.survfit
+    autolayer.summary.survfit(...)
+}
+
+
+
+
+#' @title Layers of Kaplan-Meier Curve of \link[survival]{summary.survfit} using \CRANpkg{ggplot2}
+#' 
+#' @description ..
+#' 
+#' @param object \link[survival]{summary.survfit}
+#' 
 #' @param ribbon \link[base]{logical} scalar, default `TRUE`
 #' 
 #' @param labels (optional) \link[base]{character} \link[base]{vector}
@@ -13,19 +47,12 @@
 #' 
 #' @param ... ..
 #' 
-#' @note
-#' It's very difficulty to grab the time \link[base]{units} of the response of a 
-#' \link[survival]{survfit} object.
-#' The only way is `eval(object$call$formula[[2L]], envir = eval(object$call$data))`,
-#' which is very vulnerable as `object$call$data` might be defined in some other internal functions.
-#' Therefore we leave the specification of `xlab` (ideally the time unit) to the end user.
-#' 
 #' @keywords internal
 #' @importFrom ggplot2 autolayer aes geom_ribbon geom_step scale_fill_discrete geom_point scale_colour_discrete
 #' @importFrom stats setNames
-#' @export autolayer.survfit
+#' @export autolayer.summary.survfit
 #' @export
-autolayer.survfit <- function(
+autolayer.summary.survfit <- function(
     object, 
     ribbon = TRUE,
     labels = NULL,
@@ -33,38 +60,36 @@ autolayer.survfit <- function(
     ...
 ) {
   
+  # how do I ensure `survival:::summary.survfit(., censored = TRUE)` ??
+  
   fom <- object$call$formula
   if (fom[[1L]] != '~') stop('must preserve the true `formula`')
   
-  d <- object |> 
-    summary(censored = TRUE) # ?survival:::summary.survfit
-  # `censored`, ignored if !missing(times); should the censoring times be included in the output?
-  # `extend`, ignored if missing(times); even if there are no subjects left at the end of the specified times
   if (oneminus) {
-    d <- d |> 
+    object <- object |> 
       oneminus.summary.survfit()
   }
   
-  labels <- d$strata |>
+  labels <- object$strata |>
     attr(which = 'levels', exact = TRUE)
   # NULL compatible!
   
-  id_c <- (d$n.censor > 0L)
+  id_c <- (object$n.censor > 0L)
   
   strata_nm <- if (is.symbol(fom[[3L]])) deparse1(fom[[3L]]) # else NULL
   
   return(list( 
     
     geom_step(
-      mapping = aes(x = d$time, y = d$surv, group = d$strata, colour = d$strata),
+      mapping = aes(x = object$time, y = object$surv, group = object$strata, colour = object$strata),
     ), 
     
     if (ribbon) geom_ribbon(
-      mapping = aes(x = d$time, ymax = d$upper, ymin = d$lower, group = d$strata, fill = d$strata), 
+      mapping = aes(x = object$time, ymax = object$upper, ymin = object$lower, group = object$strata, fill = object$strata), 
       alpha = .1),
     
     geom_point(
-      mapping = aes(x = d$time[id_c], y = d$surv[id_c], group = d$strata[id_c], colour = d$strata[id_c]),
+      mapping = aes(x = object$time[id_c], y = object$surv[id_c], group = object$strata[id_c], colour = object$strata[id_c]),
       shape = 3L),
     
     (if (length(labels)) scale_colour_discrete(labels = labels)),
@@ -79,8 +104,15 @@ autolayer.survfit <- function(
     )
     
   ))
-
+  
 }
+
+
+
+
+
+
+
 
 
 
@@ -145,9 +177,11 @@ nobsText.survfit <- function(x) {
 #' @param ... ..
 #' 
 #' @examples
+#' s0 = survfit(Surv(time, status) ~ 1, data = aml)
+#' s1 = survfit(Surv(time, status) ~ x, data = aml)
 #' list(
-#'  'no strata' = survfit(Surv(time, status) ~ 1, data = aml),
-#'  'one strata' = survfit(Surv(time, status) ~ x, data = aml)
+#'  'no strata' = list(s0, summary(s0, times = c(10, 25))),
+#'  'one strata' = list(s1, summary(s1, times = c(10, 25)))
 #' ) |> rmd.tzh::render_(file = 'survfit')
 #' @keywords internal
 #' @importFrom methods new
@@ -163,7 +197,7 @@ md_.survfit <- function(x, xnm, ...) {
     new(Class = 'md_lines', package = 'survival', bibentry = KaplanMeier58())
   
   z2 <- c(
-    '<details><summary>Survival Stats</summary>',
+    '<details><summary>Median Survival (& Confidence Interval)</summary>',
     '```{r}',
     '#| echo: false', 
     '#| comment: ',
@@ -179,6 +213,7 @@ md_.survfit <- function(x, xnm, ...) {
   z3 <- c(
     '```{r}',
     '#| echo: false', 
+    '#| warning: false', 
     x |>
       attr(which = 'fig-height', exact = TRUE) |> 
       sprintf(fmt = '#| fig-height: %.1f'),
@@ -198,6 +233,32 @@ md_.survfit <- function(x, xnm, ...) {
 
 
 
-
+#' @title [md_.summary.survfit]
+#' 
+#' @description ..
+#' 
+#' @param x a \link[survival]{summary.survfit} object
+#' 
+#' @param xnm ..
+#'  
+#' @param ... ..
+#' 
+#' @keywords internal
+#' @importFrom methods new
+#' @importClassesFrom rmd.tzh md_lines  
+#' @importFrom rmd.tzh md_
+#' @export md_.summary.survfit
+#' @export
+md_.summary.survfit <- function(x, xnm, ...) {
+  
+  c(
+    '```{r}',
+    '#| echo: false', 
+    xnm |> sprintf(fmt = '%s |> as_flextable.summary.survfit(which = \'surv\')'), 
+    '```'
+  ) |> 
+    new(Class = 'md_lines')
+  
+}
 
 
